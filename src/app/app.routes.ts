150d587059465c4017bd86cee1f12fcb79a7078d
Arseniy_Kuzenkov <senya57k@gmail.com>
2025-09-13 23:52:00 +0300

    fixed bug, when button lagged

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { SupportComponent } from './pages/support.component/support.component';
 import { RestaurantsListComponent } from './pages/restaurants-list.component/restaurants-list.component';
-import { RestaurantCatalogComponent } from './pages/restaurant-catalog.component/restaurant-catalog.component';
+import { RestaurantMenuComponent } from './pages/restaurant-menu.component/restaurant-menu.component';
 
 export const routes: Routes = [
 
     { path: '', component: RestaurantsListComponent, pathMatch: 'full' },
-    { path: 'list', component: RestaurantCatalogComponent },
+    { path: 'list', component: RestaurantMenuComponent },
     { path: 'support', component: SupportComponent },
 ];
